cfb99c55e70ec7431768584f136461770a061e37
Xris4o0 <xris4o0@gmail.com>
2019-10-20 13:53:14 -0400

    Email added.

--- src/servlets/EmailServlet.java
+++ src/servlets/EmailServlet.java
@@ -7,16 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/*
- *
- *  Project: StressGrammers
- * Assignment: Java Assignment 1
- * Author(s): Jason Thai, Hristo Tsvetkov, Nunkedie Steeven Wemin
- * Student Number: 101107083, 100719969, 101091788
- * Date: Oct. 20, 2019
- * Description: This is a project that can log in, register, and view a dashboard page. 
- * 
- */
 
 @WebServlet("/EmailServlet")
 public class EmailServlet extends HttpServlet {
